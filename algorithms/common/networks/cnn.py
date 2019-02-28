de97878479b2e84842d144548d75e8754368a2e4
curt-park <curt.park@medipixel.io>
2019-02-28 13:56:59 +0900

    Add late fusion mlp and cnn

--- algorithms/common/networks/cnn.py
+++ algorithms/common/networks/cnn.py
@@ -13,7 +13,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from algorithms.common.helper_functions import identity
-from algorithms.common.networks.mlp import MLP
+from algorithms.common.networks.mlp import MLP, LateFusionMLP
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -59,28 +59,34 @@ class CNN(nn.Module):
         for i, cnn_layer in enumerate(self.cnn_layers):
             self.cnn.add_module("cnn_{}".format(i), cnn_layer)
 
-    def get_last_activation_cnn(self, x: torch.Tensor) -> torch.Tensor:
-        """Get the activation of the last cnn layer."""
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Forward method implementation."""
         if len(x.size()) == 3:
             x = x.unsqueeze(0)
         x = self.cnn(x).squeeze()
         x = x.view(x.size(0), -1)
-        return x
-
-    def get_last_activation(self, x: torch.Tensor) -> torch.Tensor:
-        """Get the activation of the last hidden layer."""
-        x = self.get_last_activation_cnn(x)
-        x = self.fc_layers.get_last_activation(x)
-        return x
-
-    def forward_late_fusion(self, x: torch.Tensor, late_in: list) -> torch.Tensor:
-        """Forward method implementation."""
-        x = self.get_last_activation_cnn(x)
-        x = self.fc_layers.forward_late_fusion(x, late_in)
-        return x
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """Forward method implementation."""
-        x = self.get_last_activation_cnn(x)
         x = self.fc_layers(x)
+        return x
+
+
+class LateFusionCNN(nn.Module):
+    """Convolution neural network with late fusion inputs."""
+
+    def __init__(self, cnn_layers: List[CNNLayer], fc_layers: LateFusionMLP):
+        super(LateFusionCNN, self).__init__()
+
+        self.cnn_layers = cnn_layers
+        self.fc_layers = fc_layers
+
+        self.cnn = nn.Sequential()
+        for i, cnn_layer in enumerate(self.cnn_layers):
+            self.cnn.add_module("cnn_{}".format(i), cnn_layer)
+
+    def forward(self, x: torch.Tensor, late_in: list) -> torch.Tensor:
+        """Forward method implementation."""
+        if len(x.size()) == 3:
+            x = x.unsqueeze(0)
+        x = self.cnn(x).squeeze()
+        x = x.view(x.size(0), -1)
+        x = self.fc_layers(x, late_in)
         return x
